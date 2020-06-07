cb129aaaf5fd2fc0c673a2e4f53fb5b3bc7bd164
kevinkim <kevinkim.yhk@gmail.com>
2020-06-07 08:05:51 -0700

    made index and show for playerscontroller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,4 @@
 class PlayersController < ApplicationController
-    before_action only: [:index, :show]
     
     def index
         @players = Player.all 
@@ -7,41 +6,8 @@ class PlayersController < ApplicationController
     end 
 
     def show
-        find_player
-        render json: @player
-    end 
-
-    def create
-        @player = Player.create(player_params)
-        render json: @player
-    end
-
-
-    def update 
-        @player.update(player_params)
-        if @player.save
-          render json: @player
-        else
-          render json: { errors: @player.errors.full_messages }
-        end
-    end 
-
-    def destroy
-        player = Plyaer.find_by(id: params[:id])
-        player.destroy
-        render json: player
-    end
-
-
-    private
-
-    def player_params
-        params.permit(:name, :ppg, :rpg, :apg, :description)
-    end
-
-    def find_player
         @player = Player.find(params[:id])
-    end
-
+        render json: @player
+    end 
 
 end

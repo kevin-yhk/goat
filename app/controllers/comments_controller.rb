218812667a613eab9331cf4df97c4451b5dbe48f
kevinkim <kevinkim.yhk@gmail.com>
2020-06-07 08:05:30 -0700

    made index show and create for commentscontroller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+    def index
+        @comments = Comment.all
+        render json: @comments 
+    end
+
+    def show
+        find_comment
+        render json: @comment
+    end
+
+    def create 
+        @comment = Comment.create(user_params)
+        render json: {message: "Comment Created"}
+    end
+
+    private 
+
+    def comment_params
+        params.permit(:name, :content)
+    end
+
 end
